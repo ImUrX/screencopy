da4949dc5e2c2d10dacaeef438a17434921ecf9d
ImUrX <urielfontan2002@gmail.com>
2022-01-27 02:35:05 -0300

    change pc

--- forge/src/main/java/io/github/imurx/screenshotcopy/forge/ScreenshotEventHandler.java
+++ forge/src/main/java/io/github/imurx/screenshotcopy/forge/ScreenshotEventHandler.java
@@ -1,25 +1,16 @@
 package io.github.imurx.screenshotcopy.forge;
-
 import io.github.imurx.screenshotcopy.ScreencopyConfig;
 import io.github.imurx.screenshotcopy.ScreenshotCopy;
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.minecraft.text.TranslatableText;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenshotEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import io.github.imurx.arboard.Clipboard;
 
-@Mod(ScreenshotCopy.MOD_ID)
-public class ScreenshotCopyForge {
-    public ScreenshotCopyForge() {
-        var a = new Clipboard();
-        a.close();
-        AutoConfig.register(ScreencopyConfig.class, Toml4jConfigSerializer::new);
-        ScreenshotCopy.init();
-    }
-
+@Mod.EventBusSubscriber(modid = ScreenshotCopy.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ScreenshotEventHandler {
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onScreenshot(ScreenshotEvent ev) {
         if(ev.getScreenshotFile().exists()) return;
