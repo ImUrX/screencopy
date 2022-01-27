9e75a9059dfaba941b308435d4bc866461ab28c4
ImUrX <urielfontan2002@gmail.com>
2022-01-26 21:48:02 -0300

    forge side in progress

--- forge/src/main/java/io/github/imurx/screenshotcopy/forge/ScreenshotCopyForge.java
+++ forge/src/main/java/io/github/imurx/screenshotcopy/forge/ScreenshotCopyForge.java
@@ -1,11 +1,18 @@
 package io.github.imurx.screenshotcopy.forge;
 
+import io.github.imurx.screenshotcopy.ScreencopyConfig;
 import io.github.imurx.screenshotcopy.ScreenshotCopy;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.minecraftforge.fml.common.Mod;
+import io.github.imurx.arboard.Clipboard;
 
 @Mod(ScreenshotCopy.MOD_ID)
 public class ScreenshotCopyForge {
     public ScreenshotCopyForge() {
-
+        var a = new Clipboard();
+        a.close();
+        AutoConfig.register(ScreencopyConfig.class, Toml4jConfigSerializer::new);
+        ScreenshotCopy.init();
     }
 }
