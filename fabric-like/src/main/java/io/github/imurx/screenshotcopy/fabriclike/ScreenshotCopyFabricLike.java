e217330f31e962aecd94b6823a3fedb2af04d7dc
Uriel <imurx@proton.me>
2025-07-11 17:30:18 -0400

    backport #15 fix

--- fabric-like/src/main/java/io/github/imurx/screenshotcopy/fabriclike/ScreenshotCopyFabricLike.java
+++
@@ -1,9 +0,0 @@
-package io.github.imurx.screenshotcopy.fabriclike;
-
-import io.github.imurx.screenshotcopy.fabriclike.ScreenshotCopyFabricLike;
-
-public class ScreenshotCopyFabricLike {
-    public static void init() {
-
-    }
-}
